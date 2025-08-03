810c8da3c904d42e60c9134f45cb22c80b6d89ee
arif-almusharafi <arif.almusharafi@gmail.com>
2025-08-03 15:07:25 +0300

    clean fills

---
+++ test.py
@@ -0,0 +1,16 @@
+# house = [["hallway", 11.25],
+#          ["kitchen", 18.0],
+#          ["living room", 20.0],
+#          ["bedroom", 10.75],
+#          ["bathroom", 9.50]]
+
+# # Subset the house list
+# print(house[4][1])
+import matplotlib.pyplot as plt
+
+plt.plot()
+
+dd = {"ss"} =2
+
+print(type(dd))
+
